6d51e3ba36005e69007a7bae8cca64f5abb77945
WuL0og <2992782994@qq.com>
2026-07-18 15:26:04 +0800

    The basic functions of the login page have been completed, and the cross-origin issue has been resolved using Vite proxy. Now all requests can be executed normally.

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus';
+import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
-  baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+  baseURL: '/api',
   timeout: 5000
 })
 
@@ -11,6 +13,11 @@ httpInstance.interceptors.request.use(config => {
 }, e => Promise.reject(e))
 
 httpInstance.interceptors.response.use(res => res.data, e => {
+  ElMessage({
+    type: 'warning',
+    message:e.response.data.message
+
+  })
   return Promise.reject(e)
 })
 
